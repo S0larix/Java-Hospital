0461b6da61cf92c9b25ba2649f9aeb68c762a3d3
S0larix <clement.raibaud@gmail.com>
2015-04-26 11:11:20 +0200

    Création des classes
    Création des classes de bases avec la classe generique

---
+++ Gestion_Hospital/src/Classes/docteur.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+/**
+ *
+ * @author Clément
+ */
+public class docteur extends employe{
+    private int numero;
+    private String specialite;
+    
+}
---
+++ Gestion_Hospital/src/Classes/hospitalisation.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+/**
+ *
+ * @author Clément
+ */
+public class hospitalisation extends generique{
+    private int no_malade;
+    private int code_service;
+    private int no_chambre;
+    private int lit;
+    
+}
