9952117275ad19e4d211cfe255610749c03be03a
cot <cot@ece.fr>
2015-04-29 13:35:27 +0200

    début partie màj

---
+++ Gestion_Hospital/src/Mise_a_jour/Opération_bdd.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mise_a_jour;
+import Interface.*;
+import Classes.*;
+import java.util.ArrayList;
+/**
+ *
+ * @author Lisa
+ */
+public class Opération_bdd {
+    public ArrayList <generique> ajouter_entité()
+    {
+        ArrayList <generique> tab;
+        generique gen;
+        //on demande à l'utilisateur de saisir les infos de l'entité
+        gen=demander_utilisateur();
+        //
+        
+        return tab;
+    }
+    
+    public ArrayList <generique> supprimer_entité()
+    {
+        ArrayList <generique> tab;
+        return tab;
+    }
+    
+    public ArrayList <generique> modifier_entité()
+    {
+        ArrayList <generique> tab;
+        return tab;
+    }
+}
