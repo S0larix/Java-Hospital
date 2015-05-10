b2e1dbe593b37c85f9f09486704d82f802dfa069
florianverbeke <florian.verbeke@yahoo.fr>
2015-05-10 19:14:13 +0200

    voila link fini monsieur jérôme
    Par contre j'ai du commenté une de tes lignes pour build les changements
    :D

--- Gestion_Hospital/src/Interface/MenuRecherche.java
+++ Gestion_Hospital/src/Interface/MenuRecherche.java
@@ -23,10 +23,8 @@ public class MenuRecherche extends javax.swing.JFrame {
     private  String LoginBDD;
     private  String PasswordBDD;
     
-    
 
-        
-       
+  
     public MenuRecherche(String LoginECE, String PasswordECE, String LoginBDD, String PasswordBDD)
     {
         
@@ -134,10 +132,41 @@ public class MenuRecherche extends javax.swing.JFrame {
         this.dispose();
         SousMenutri sous_menu_tri = new SousMenutri(LoginECE,PasswordECE,LoginBDD,PasswordBDD);
         sous_menu_tri.setVisible(true);
+        
     }//GEN-LAST:event_BoutonTriActionPerformed
 
 
+public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(SousMenuRequete.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(SousMenuRequete.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(SousMenuRequete.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(SousMenuRequete.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
 
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MenuRecherche("a","b","c","d").setVisible(true);
+            }
+        });
+    }
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton BoutonReporting;
     private javax.swing.JButton BoutonRetourMenu;
